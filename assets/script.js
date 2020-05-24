96b7eed2b3db78ab931466c36026d05e28d16601
noisette <kasusky.music@gmail.com>
2020-05-23 22:14:33 -0700

    Completed basic html. Initialized firebase.

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+ // Script for train scheduler // 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDwX_n0FxGWyhQiGDn-A2xqS8SXPSTrPyg",
+    authDomain: "choo-choo-choose-me.firebaseapp.com",
+    databaseURL: "https://choo-choo-choose-me.firebaseio.com",
+    projectId: "choo-choo-choose-me",
+    storageBucket: "choo-choo-choose-me.appspot.com",
+    messagingSenderId: "1018852599451",
+    appId: "1:1018852599451:web:b796e46c53447e5d26c83e",
+    measurementId: "G-DYD2KX0J2Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
